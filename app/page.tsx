6145187e48f8ca0995a6b991d9c530ea3f30cd65
Paristoni <paristoni93@gmail.com>
2024-03-07 00:45:14 +0700

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex justify-center gap-4 bg-orange-300">
-    <Link href="/home"> HOME </Link>
+    <Link href="/home"> Haloo </Link>
     <Link href="/about"> ABOUT </Link>
     <Link href="/service"> Service </Link>
     <Link href="/teams">Teams</Link>
